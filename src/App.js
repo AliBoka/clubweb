f3f9e11be3423d441d187a2ae91014ad49e0c42c
AliBoka <aliboka71@gmail.com>
2022-03-30 21:07:22 +0430

    Add a gradient background to the app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-full" style={{background: 'linear-gradient(180deg, #333333 0%, #090707 11.98%)',
+      boxShadow : '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>
+    
     </div>
   );
 }

0faf461be705e5aee7c76a6beb07f6284c474cc3
AliBoka <aliboka71@gmail.com>
2022-03-31 01:08:28 +0430

    install react-scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="h-full" style={{background: 'linear-gradient(180deg, #333333 0%, #090707 11.98%)',
+    <div className="h-screen" style={{background: 'linear-gradient(180deg, #333333 0%, #090707 11.98%)',
       boxShadow : '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>
-    
+    <Navbar/>
     </div>
   );
 }

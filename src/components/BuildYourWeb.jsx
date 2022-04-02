b1bdec211415f4d5fc642511af98c109cf426590
AliBoka <aliboka71@gmail.com>
2022-04-02 22:59:34 +0430

    add Cards section to Build component

--- src/components/BuildYourWeb.jsx
+++ src/components/BuildYourWeb.jsx
@@ -16,7 +16,47 @@ const BuildYourWeb = () => {
           faster
         </p>
       </div>
+      {/* *********** Cards ************ */}
 
+      <div className="w-full flex flex-col sm:flex-row gap-3 mt-12 mb-6">
+        <div className="flex flex-col py-6 sm:px-6 px-8 bg-[#C96408] w-full rounded justify-center items-center">
+          <img
+            src={Cartimg}
+            alt="cart-pic"
+            className="w-12 h-12 shadow-[2px_2px_10px_rgba(0,0,0,0.3)] rounded-lg "
+          />
+          <p className="text-lg font-semiboldbold py-4 ">
+            E-commerce website
+          </p>
+          <p className="text-sm font-extralight pb-4">
+            Engage a developer to create a customized e-commerce site at low
+          </p>
+        </div>
+        <div className="flex flex-col py-6 sm:px-6 px-8 bg-[#7937BB] w-full rounded justify-center items-center">
+          <img
+            src={Searchimg}
+            alt="cart-pic"
+            className="w-12 h-12 shadow-[2px_2px_10px_rgba(0,0,0,0.3)] rounded-lg "
+          />
+          <p className="text-lg font-semiboldbold py-4 ">Portfolio website</p>
+          <p className="text-sm font-extralight pb-4">
+            A portfolio website is a profile website by nature, so this is the
+            place to get a deveoper
+          </p>
+        </div>
+        <div className="flex flex-col py-6 sm:px-6 px-84 bg-[#3775ED] w-full rounded justify-center items-center">
+          <img
+            src={Cardimg}
+            alt="cart-pic"
+            className="w-12 h-12 shadow-[2px_2px_10px_rgba(0,0,0,0.3)] rounded-lg "
+          />
+          <p className="text-lg font-semiboldbold py-4 ">Marketing website</p>
+          <p className="text-sm font-extralight pb-4">
+            This is a website created with lot of marketing advantage. Do you
+            have one?
+          </p>
+        </div>
+      </div>
      
     </div>
   );

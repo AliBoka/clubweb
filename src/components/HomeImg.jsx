a2f89327e9a935adb1b035949e0adbf692f8d5e4
ali boka <97402165+AliBoka@users.noreply.github.com>
2022-04-07 17:37:29 +0430

    Update HomeImg.jsx

--- src/components/HomeImg.jsx
+++ src/components/HomeImg.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import homeImg from "../assets/homeimgMobile.png";
 import homeImg2 from "../assets/homepage-img.png";
-import { CgChevronDoubleDown } from "react-icons/cg";
-import Link from "react-scroll/modules/components/Link";
 const HomeImg = () => {
- const [mobile, setMobile] = useState(window.innerWidth > 640 ? false : true); 
+ const [mobile, setMobile] = useState(false); 
   return (
     <div name="homeimg" className="flex items-center h-screen py-20 bg-[#090707] w-full">
      {
@@ -15,14 +13,13 @@ const HomeImg = () => {
      { 
      mobile ? (
       
+       <img src={homeImg} alt="home-img" className=" w-full" />
+       ) : (
         <img
           src={homeImg2}
           alt="home-img"
           className="sm:object-contain w-full 2xl:object-cover"
         />
-      
-      ) : (
-       <img src={homeImg} alt="home-img" className=" w-full h-[380px]" />
       )
   }
      

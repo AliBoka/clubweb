d80647c107651ad473bc876e86a17936e2920500
AliBoka <aliboka71@gmail.com>
2022-04-23 14:17:29 +0430

    fix home img

--- src/components/HomeImg.jsx
+++ src/components/HomeImg.jsx
@@ -1,30 +1,18 @@
-import React, { useState } from "react";
-import homeImg from "../assets/homeimgMobile.png";
+import React from "react";
 import homeImg2 from "../assets/homepage-img.png";
-import { CgChevronDoubleDown } from "react-icons/cg";
-import Link from "react-scroll/modules/components/Link";
 const HomeImg = () => {
-  const [mobil, setMobil] = useState(false);
- 
   return (
-    <div name="homeimg" className="flex items-center h-screen py-20 bg-[#090707] w-full">
-     { 
-     window.innerWidth > 412 ? (
-      
+    <div
+      name="homeimg"
+      className="flex items-center h-auto  py-20 bg-[#090707] w-full"
+    >
+     
         <img
           src={homeImg2}
           alt="home-img"
-          className="sm:object-contain w-full 2xl:object-cover"
+          className="sm:object-contain w-full h-64 sm:h-auto 2xl:object-cover"
         />
-      
-      ) : (
-       <img src={homeImg} alt="home-img" className=" w-full h-[380px]" />
-      )
-  }
-      {/* <Link to="homeimg" smooth={true} className="sm:absolute bottom-2 flex flex-col rounded-full h-10 w-10 bg-white justify-center items-center mb-2 cursor-pointer">
-          <CgChevronDoubleDown className="text-blue-600 text-2xl
-           "/>
-      </Link> */}
+    
     </div>
   );
 };
